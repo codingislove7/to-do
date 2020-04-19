ad51cc7ec38872ae4d4174e691dfd80d1acfcb86
Behnam Sheykhe <learndlt@gmail.com>
2020-04-19 15:00:51 +0430

    add mongoose and validator

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,30 @@
 // CRUD create read update delete
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "to-do";
 
-MongoClient.connect(connectionURL, {useNewUrlParser : true}, (error, client)=>{
-    if(error){
-        return console.log("unable to connect to database.");
+// setup connection with database
+MongoClient.connect(
+  connectionURL,
+  { useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("unable to connect to database.");
     }
-    console.log("connected correctly");
-})
+    const db = client.db(databaseName);
+    db.collection("users")
+      .deleteOne({
+        name: "sssorss",
+      })
+      .then((result) => {
+        console.log("====================================");
+        console.log(result.deletedCount);
+        console.log("====================================");
+      })
+      .catch((error) => {
+        console.log("====================================");
+        console.log(error);
+        console.log("====================================");
+      });
+  }
+);
